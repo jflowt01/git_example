e66363dff0ee8d5d1fc992ddf9d1e5460fb5f884
Jair Lopez Perez <jair.lopez@ulv.edu.mx>
2024-11-18 08:14:48 -0600

    Añadida la edad en el saludo

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("Bienvenido al programa de salud");
+function saludar(nombre) {
+    return `Hola, ${nombre}! ¿Como estas?`;
+}
+const nombreUsuario = "Estudiante";
+console.log(saludar(nombreUsuario));
+
+console.log("Fin");
+
+
